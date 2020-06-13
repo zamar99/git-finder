f487f664eb1e4c054fa853a9f9ee87b1f65712f7
Zamar Zahid <zamerzahid@gmail.com>
2020-06-14 00:31:55 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Layout/Navbar';
+import { Alert } from './Components/Layout/Alert';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { About } from './Components/pages/About';
+import User from './Components/users/User';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+import { Home } from './Components/pages/Home';
+import { NotFound } from './Components/pages/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Layout/Alert.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+
+export const Alert = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { alert } = alertContext;
+
+  return (
+    alert != null && (
+      <div>
+        <div className={`alert alert-${alert.type}`}>
+          <i className="fas fa-info-circle" />
+          {alert.msg}
+        </div>
+      </div>
+    )
+  );
+};
